4255196763fa55b7e5db21c202d8c67f6bc180dd
Aaron Van Bokhoven <bokhoven@gmail.com>
2021-06-14 15:32:24 -1000

    Add support for incoming requests
    This adds support for the incoming request payload channel
    and the error channel if we fail to start the web renderer server.

--- pkg/renderer/web.go
+++ pkg/renderer/web.go
@@ -5,36 +5,53 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
+	"sync"
 
+	"github.com/aaronvb/request_hole/pkg/protocol"
 	"github.com/gorilla/mux"
 	"github.com/pterm/pterm"
 )
 
 type Web struct {
-	RendererFlagData
-	requests []RequestPayload
+	Port     int
+	requests []protocol.RequestPayload
 }
 
-func (web *Web) Start(req chan RequestPayload) {
-	printFlagData(web.RendererFlagData)
+func (web *Web) Start(wg *sync.WaitGroup, rp chan protocol.RequestPayload, q chan int, e chan int) {
+	// Initialize requests as an empty slice so that we can return a proper json empty array
+	// if no values have been appended.
+	web.requests = make([]protocol.RequestPayload, 0)
 
-	// TODO: need to spin up a WS server as well
-	addr := fmt.Sprintf("localhost:%d", web.WebPort)
-	errorLog := log.New(&PrinterLog{Prefix: pterm.Error}, "", 0)
+	addr := fmt.Sprintf("localhost:%d", web.Port)
+	errorLog := log.New(&httpErrorLog{}, "", 0)
 
 	srv := &http.Server{
 		Addr:     addr,
 		ErrorLog: errorLog,
 		Handler:  web.routes(),
 	}
-	r := <-req
-	web.IncomingRequest(r)
-	err := srv.ListenAndServe()
-	web.Fatal(err)
+
+	defer wg.Done()
+
+	go func() {
+		err := srv.ListenAndServe()
+		str := pterm.Error.WithShowLineNumber(false).Sprintf("Web: %s\n", err)
+		pterm.Printo(str) // Overwrite last line
+		e <- 1
+	}()
+
+	for {
+		select {
+		case r := <-rp:
+			web.incomingRequest(r)
+		case <-q:
+			close(rp)
+			return
+		}
+	}
 }
 
-// routes handles the routes for our HTTP server and currently accepts any path.
+// routes handles routes for our web UI.
 func (web *Web) routes() http.Handler {
 	r := mux.NewRouter()
 	r.HandleFunc("/requests", web.requestsHandler).Methods("GET")
@@ -42,22 +59,25 @@ func (web *Web) routes() http.Handler {
 	return r
 }
 
+// requestsHandler returns an array of incoming requests to our protocol server.
 func (web *Web) requestsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(web.requests)
 }
 
-// Fatal will use the Error prefix to render the error and then exit the CLI.
-func (web *Web) Fatal(err error) {
-	pterm.Error.WithShowLineNumber(false).Println(err)
-	os.Exit(1)
-}
-
-func (web *Web) incomingRequest(req RequestPayload) {
-	// TODO: this is not thread safe, use mutex
+// incomingRequest is called when we receive a RequestPayload over the channel
+// from the protocol server. This will add it to the request slice which our web ui
+// will serve as JSON and be consumed on the front end.
+func (web *Web) incomingRequest(req protocol.RequestPayload) {
 	web.requests = append(web.requests, req)
 }
 
-func (web *Web) IncomingRequestHeaders(headers map[string][]string) {
+// httpErrorLog implements the logger interface.
+type httpErrorLog struct{}
 
+// Write let's us override the logger required for http errors and
+// prints to the terminal using pterm.
+func (e *httpErrorLog) Write(b []byte) (n int, err error) {
+	pterm.Error.WithShowLineNumber(false).Println(string(b))
+	return len(b), nil
 }
