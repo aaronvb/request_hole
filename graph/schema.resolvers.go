14047969cdfe0f60840c2b34b27d3ab9403fb070
Aaron Van Bokhoven <bokhoven@gmail.com>
2021-06-28 18:57:34 -1000

    Add subscription resolver
    This does a few things:
    
    Creates a uuid for the browser connection.
    
    Creates a go routine that handles the browser connection close,
    which will delete the browser uuid from the observer channel.
    
    Adds the browser uuid to the channel map which we can send
    messages to.

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -9,6 +9,7 @@ import (
 	"github.com/aaronvb/request_hole/graph/generated"
 	"github.com/aaronvb/request_hole/graph/model"
 	"github.com/aaronvb/request_hole/pkg/protocol"
+	"github.com/google/uuid"
 )
 
 func (r *mutationResolver) ClearRequests(ctx context.Context) (bool, error) {
@@ -25,11 +26,35 @@ func (r *queryResolver) ServerInfo(ctx context.Context) (*model.ServerInfo, erro
 	return r.Info, nil
 }
 
+func (r *subscriptionResolver) Request(ctx context.Context) (<-chan *protocol.RequestPayload, error) {
+	// Generate UUID for browser connection
+	id := uuid.New().String()
+
+	// Go routine to handle deleting channel if browser connection is closed.
+	go func() {
+		<-ctx.Done()
+		r.mu.Lock()
+		delete(*r.RequestPayloadObserver, id)
+		r.mu.Unlock()
+	}()
+
+	requests := make(chan *protocol.RequestPayload, 1)
+	r.mu.Lock()
+	(*r.RequestPayloadObserver)[id] = requests
+	r.mu.Unlock()
+
+	return requests, nil
+}
+
 // Mutation returns generated.MutationResolver implementation.
 func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
 
 // Query returns generated.QueryResolver implementation.
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
+// Subscription returns generated.SubscriptionResolver implementation.
+func (r *Resolver) Subscription() generated.SubscriptionResolver { return &subscriptionResolver{r} }
+
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
+type subscriptionResolver struct{ *Resolver }
