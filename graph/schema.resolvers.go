e22c26e94b27eef263efd1a983c5880da624fc79
Aaron Van Bokhoven <bokhoven@gmail.com>
2021-06-28 18:54:01 -1000

    Update resolvers
    Added a ClearRequest resolver which clears the RequestPayloads
    slice in memory.
    
    Added ServerInfo resolver which returns our graphql model.

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -7,14 +7,29 @@ import (
 	"context"
 
 	"github.com/aaronvb/request_hole/graph/generated"
+	"github.com/aaronvb/request_hole/graph/model"
 	"github.com/aaronvb/request_hole/pkg/protocol"
 )
 
-func (r *queryResolver) Requests(ctx context.Context) ([]protocol.RequestPayload, error) {
+func (r *mutationResolver) ClearRequests(ctx context.Context) (bool, error) {
+	reqs := *r.RequestPayloads
+	*r.RequestPayloads = reqs[:0]
+	return true, nil
+}
+
+func (r *queryResolver) Requests(ctx context.Context) ([]*protocol.RequestPayload, error) {
 	return *r.RequestPayloads, nil
 }
+
+func (r *queryResolver) ServerInfo(ctx context.Context) (*model.ServerInfo, error) {
+	return r.Info, nil
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
 
 // Query returns generated.QueryResolver implementation.
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
+type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
