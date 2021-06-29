7b1254a0132b617f395a18c8747578e5ce9dee83
Aaron Van Bokhoven <bokhoven@gmail.com>
2021-06-28 19:10:49 -1000

    Add link to handle queries and subscriptions

--- web/src/index.js
+++ web/src/index.js
@@ -3,15 +3,47 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
-} from "@apollo/client";
+  split,
+  HttpLink
+} from '@apollo/client';
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:8081/query',
+});
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:8081/query`,
+  options: {
+    reconnect: true,
+  },
+});
+
+// The split function takes three parameters:
+//
+// * A function that's called for each operation to execute
+// * The Link to use for an operation if the function returns a "truthy" value
+// * The Link to use for an operation if the function returns a "falsy" value
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
 
 const client = new ApolloClient({
-  uri: 'http://localhost:8081/query',
-  cache: new InMemoryCache(),
+  link: splitLink,
+  cache: new InMemoryCache()
 });
 
 ReactDOM.render(
