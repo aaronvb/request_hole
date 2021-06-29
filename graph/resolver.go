dc1d54a0952e00fc3a26a24c77100d90e3dfec8d
Aaron Van Bokhoven <bokhoven@gmail.com>
2021-06-28 18:48:24 -1000

    Update resolver structs
    Updated RequestPayloads to use slice element pointer
    
    Added RequestPayloadObserver for our subscription websocket
    payload
    
    Added Info for our ServerInfo query

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,11 +1,19 @@
 package graph
 
-import "github.com/aaronvb/request_hole/pkg/protocol"
+import (
+	"sync"
+
+	"github.com/aaronvb/request_hole/graph/model"
+	"github.com/aaronvb/request_hole/pkg/protocol"
+)
 
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
 type Resolver struct {
-	RequestPayloads *[]protocol.RequestPayload
+	RequestPayloads        *[]*protocol.RequestPayload
+	RequestPayloadObserver *map[string]chan *protocol.RequestPayload
+	Info                   *model.ServerInfo
+	mu                     sync.Mutex
 }
